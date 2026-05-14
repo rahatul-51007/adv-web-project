29e019f9290556d7a6e67064f950d1782646d45c
rahatul-51007 <rahatul51007@gmail.com>
2026-05-14 14:37:59 +0600

    Roles guard implemented

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,8 +2,9 @@ import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserRegistrationDto } from './user-registration.dto';
 import { Public } from './public.decorator';
-import { User } from 'src/users/users.entity';
+import { Role, User } from 'src/users/users.entity';
 import { CurrentUser } from './current-user.decorator';
+import { Roles } from './roles.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -23,25 +24,25 @@ export class AuthController {
     }
 
     @Get('current-user')
-    
+    @Roles(Role.ADMIN, Role.MEMBER)
     getProfile(@CurrentUser() user: User) {
         return user;
     }
 
     @Get('email')
-
+    @Roles(Role.ADMIN, Role.MEMBER)
     getEmail(@CurrentUser('email') email: string) {
         return { email };
     }
 
     @Get('all-users')
-
+    @Roles(Role.ADMIN)
     async getAllUsers() {
         return this.authService.findAllUsers();
     }
 
     @Get('user/:id')
-
+    @Roles(Role.ADMIN)
     async getUserById(@Param('id') id: number) {
         return this.authService.findUserById(id);
     }
