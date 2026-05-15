2fdf9761b5fcfa93cb06749d5288267fc8c3fe9e
rahatul-51007 <rahatul51007@gmail.com>
2026-05-15 11:23:26 +0600

    password reset added

---
+++ backend/src/auth/password-reset.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import { User } from '../users/users.entity';
+
+@Entity('password_resets')
+export class PasswordReset {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  token: string;
+
+  @Column()
+  email: string;
+
+  @Column({ default: false })
+  isUsed: boolean;
+
+  @Column()
+  expiresAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => User)
+  user: User;
+}
